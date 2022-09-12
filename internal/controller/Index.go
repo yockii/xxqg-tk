65e426a30ecfe62409175620ca22dd51fc2bc466
yockii <xuyuqi@gmail.com>
2022-09-12 11:52:23 +0800

    更新

--- internal/controller/Index.go
+++ internal/controller/Index.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"sort"
 	"strings"
 
 	"github.com/gofiber/fiber/v2"
@@ -8,13 +9,22 @@ import (
 
 	"xxqg-tk/internal/model"
 	"xxqg-tk/pkg/database"
+	"xxqg-tk/pkg/domain"
 	"xxqg-tk/pkg/server"
 	"xxqg-tk/pkg/util"
 )
 
 type Bank struct {
-	Question string `json:"q,omitempty" form:"q"`
-	Answer   string `json:"a,omitempty" form:"a"`
+	Question    string `json:"q,omitempty" form:"q"`
+	Options     string `json:"o,omitempty" form:"o"`
+	Answer      string `json:"a,omitempty" form:"a"`
+	WrongAnswer string `json:"wa,omitempty" form:"wa"`
+}
+
+func (qb *Bank) SortOptions() {
+	options := strings.Split(qb.Options, "|")
+	sort.Strings(options)
+	qb.Options = strings.Join(options, "|")
 }
 
 func InitRouter() {
@@ -24,16 +34,29 @@ func InitRouter() {
 		if err := ctx.BodyParser(bank); err != nil {
 			return fiber.ErrBadRequest
 		}
-		questionWithOptions := strings.Split(bank.Question, "|")
-
-		qb := &model.QuestionBank{Question: questionWithOptions[0], Options: strings.Join(questionWithOptions[1:], "|")}
-		if exist, err := database.DB.Get(qb); err != nil {
+		//questionWithOptions := strings.Split(bank.Question, "|")
+		bank.SortOptions()
+		qb := &model.QuestionBank{Options: bank.Options}
+		//qb := &model.QuestionBank{Options: strings.Join(questionWithOptions[1:], "|")}
+		if exist, err := database.DB.Where("question like ?", bank.Question+"%").Get(qb); err != nil {
+			//if exist, err := database.DB.Where("question like ?", questionWithOptions[0]+"%").Get(qb); err != nil {
 			logger.Error(err)
-			return ctx.SendString("")
+			return ctx.JSON(&domain.CommonResponse{})
 		} else if exist {
-			return ctx.SendString(qb.Answer)
+			return ctx.JSON(&domain.CommonResponse{
+				Data: &model.QuestionBank{
+					Answer:      qb.Answer,
+					WrongAnswer: qb.WrongAnswer,
+				},
+			})
 		}
-		return ctx.SendString("")
+		database.DB.Insert(&model.QuestionBank{
+			Question: bank.Question,
+			//Question:    questionWithOptions[0],
+			Options: bank.Options,
+			//WrongAnswer: bank.WrongAnswer,
+		})
+		return ctx.JSON(&domain.CommonResponse{})
 	})
 
 	qa.Post("/queryLike", func(ctx *fiber.Ctx) error {
@@ -41,16 +64,24 @@ func InitRouter() {
 		if err := ctx.BodyParser(bank); err != nil {
 			return fiber.ErrBadRequest
 		}
-		questionWithOptions := strings.Split(bank.Question, "|")
+		//questionWithOptions := strings.Split(bank.Question, "|")
+		bank.SortOptions()
 
-		qb := &model.QuestionBank{Question: questionWithOptions[0], Options: strings.Join(questionWithOptions[1:], "|")}
-		if exist, err := database.DB.Where("question like ?", questionWithOptions[0]).Get(qb); err != nil {
+		qb := &model.QuestionBank{Options: bank.Options}
+		//qb := &model.QuestionBank{Question: questionWithOptions[0], Options: strings.Join(questionWithOptions[1:], "|")}
+		if exist, err := database.DB.Where("question like ?", bank.Question).Get(qb); err != nil {
 			logger.Error(err)
-			return ctx.SendString("")
+			return ctx.JSON(&domain.CommonResponse{})
 		} else if exist {
-			return ctx.SendString(qb.Answer)
+			return ctx.JSON(&domain.CommonResponse{
+				Data: &model.QuestionBank{
+					Answer:      qb.Answer,
+					WrongAnswer: qb.WrongAnswer,
+				},
+			})
 		}
-		return ctx.SendString("")
+
+		return ctx.JSON(&domain.CommonResponse{})
 	})
 
 	qa.Post("/add", func(ctx *fiber.Ctx) error {
@@ -59,23 +90,37 @@ func InitRouter() {
 			logger.Error(err)
 			return ctx.SendStatus(fiber.StatusPreconditionFailed)
 		}
-		questionWithOptions := strings.Split(bank.Question, "|")
-		if c, err := database.DB.Count(&model.QuestionBank{Question: questionWithOptions[0], Options: strings.Join(questionWithOptions[1:], "|")}); err != nil {
+		bank.SortOptions()
+		dbBank := &model.QuestionBank{
+			Question: bank.Question,
+			Options:  bank.Options,
+		}
+		if exists, err := database.DB.Get(dbBank); err != nil {
 			logger.Error(err)
 			return ctx.SendStatus(fiber.StatusPreconditionFailed)
-		} else if c > 0 {
-			database.DB.Update(&model.QuestionBank{Answer: bank.Answer}, &model.QuestionBank{Question: questionWithOptions[0], Options: strings.Join(questionWithOptions[1:], "|")})
-			return ctx.SendString("ok")
+		} else if exists {
+			if strings.Contains(dbBank.WrongAnswer, bank.WrongAnswer) {
+				bank.WrongAnswer = ""
+			} else {
+				bank.WrongAnswer = dbBank.WrongAnswer + "|" + bank.WrongAnswer
+			}
+			database.DB.ID(dbBank.Id).Update(
+				&model.QuestionBank{
+					Answer:      bank.Answer,
+					WrongAnswer: bank.WrongAnswer,
+				})
+			return ctx.JSON(&domain.CommonResponse{})
 		}
 		if _, err := database.DB.Insert(&model.QuestionBank{
-			Id:       util.GenerateDatabaseID(),
-			Question: questionWithOptions[0],
-			Options:  strings.Join(questionWithOptions[1:], "|"),
-			Answer:   bank.Answer,
+			Id:          util.GenerateDatabaseID(),
+			Question:    bank.Question,
+			Options:     bank.Question,
+			Answer:      bank.Answer,
+			WrongAnswer: bank.WrongAnswer,
 		}); err != nil {
 			logger.Error(err)
 			return ctx.SendStatus(fiber.StatusPreconditionFailed)
 		}
-		return ctx.SendString("ok")
+		return ctx.JSON(&domain.CommonResponse{})
 	})
 }
